14d6db87aee0ab52c67fdd1c94684e82c3d91efc
TmHive <rmush8713@gmail.com>
2026-06-12 19:22:50 +0300

    12

--- src/kv.c
+++ src/kv.c
@@ -18,6 +18,47 @@ size_t hash(char *value, int capacity){
     
 }
 
+int kv_free(kv_t *db){
+    if (!db) return -1;
+
+    for (int i = 0; i < db->capacity; i++){
+        kv_entry_t *entry = &db->entries[i];
+        if (entry->key && entry->key != (void*)TOMBSTONE){
+            free(entry->key);
+            free(entry->value);
+            entry->key = NULL;
+            entry->value = NULL;
+        }
+    }
+    free(db->entries);
+    free(db);
+    return 0;   
+}
+
+int kv_delete(kv_t *db, char *key){
+    if (!db || !key) return -1;
+
+    size_t indx = hash(key, db->capacity);
+
+    for (int i = 0; i < db->capacity - 1; i++){
+        size_t real_indx = (indx + i) % db->capacity;
+
+        kv_entry_t *entry = &db->entries[real_indx];
+
+        if (entry->key == NULL) return -1;
+
+        if (entry->key != TOMBSTONE && !strcmp(entry->key, key)){
+            free(entry->key);
+            free(entry->value);
+            entry->key = TOMBSTONE;
+            db->count--;
+            return real_indx;
+        }
+
+    }
+    return -1;
+}
+
 int kv_put(kv_t *db, char *key, char *value){
     if (!db || !key || !value) return -1;
 
@@ -51,6 +92,23 @@ int kv_put(kv_t *db, char *key, char *value){
 
     }
     return -2;
+}
+
+char  *kv_get(kv_t *db, char *key){
+    if (!db|| !key) return NULL;
+    size_t indx = hash(key, db->capacity);
+
+    for (int i = 0; i < db->capacity - 1; i++){
+        size_t real_indx = (indx + i) % db->capacity;
+
+        kv_entry_t *entry = &db->entries[real_indx];
+        if (entry->key == NULL) return NULL;
+
+        if (entry->key && entry->key != (void*)TOMBSTONE && !strcmp(entry->key, key)){
+            return entry->value;
+        }
+    }
+    return NULL;
 }
 
 kv_t *kv_init(size_t capacity){
